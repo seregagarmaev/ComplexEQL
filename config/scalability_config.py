fc81483ece7bd0bf28bb8b008cc41c4ae18dba34
Sergei Garmaev <garmaev@enac-imos-gpu01>
2026-01-27 17:35:29 +0000

    OPERON scalability results

--- config/scalability_config.py
+++ config/scalability_config.py
@@ -24,46 +24,25 @@ class DataConfig:
 
 @dataclass(frozen=True)
 class OperonConfig:
-    # Experiment scope
     kinds: tuple[str, ...] = ("P3",)
-
-    # If len(seeds) < runs_per_d, the script will fall back to range(runs_per_d)
     runs_per_d: int = 5
     seeds: tuple[int, ...] = (0,)
 
-    # String extraction
     sym_decimals: int = 15
-
-    # Reporting
     report_csv_path: str = "reports/operon_scalability.csv"
 
-    # Plotting
-    y_log_scale: bool = True
-    x_label: str = "Number of Input Variables"
-    y_label: str = "MSE"
-
-    # Operator library
     allowed_symbols: str = "add,sub,mul,square,constant,variable"
 
-    # Evolution / search parameters
     generations: int = 1000
-    population_size: int = 1000
-    max_length: int = 20
+    population_size: int = 10000
+    max_length: int = 50
     max_depth: int = 10
 
-    # Local optimization
-    optimizer: str = "lm"
-    optimizer_iterations: int = 50
-    local_search_probability: float = 0.05
-    lamarckian_probability: float = 0.5
-
-    # Objectives and selection
-    objectives: tuple[str, ...] = ("mse", "length")
+    objectives: tuple[str, ...] = ("mse",)
     model_selection_criterion: str = "mean_squared_error"
 
-    # Logging
-    verbose: int = 1
     print_best_expression: bool = True
+    n_threads = 5
 
 
 @dataclass
